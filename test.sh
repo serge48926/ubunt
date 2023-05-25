1df00424cb4437b25d79f03aff250d8dfa1e6b7d
serge48926 <130587178+serge48926@users.noreply.github.com>
2023-05-25 11:59:47 +0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,1 @@
-apt update -y
-apt purge needrestart -y
-apt-mark hold linux-image-generic linux-headers-generic openssh-server snapd
-apt upgrade -y
-apt -y install unzip vnstat htop screen mc
-
-sudo -s
 wget -c https://download.npool.io/npool.sh -O npool.sh && sudo chmod +x npool.sh && sudo ./npool.sh 5tqsd2ayZrGa28lQ
